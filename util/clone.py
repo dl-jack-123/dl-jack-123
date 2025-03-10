c028eac4a0b2ac3041c01e87f1889046cea8525d
dl <jick155@yahoo.com.tw>
2025-03-10 20:56:33 +0800

    test:     clone func

--- util/clone.py
+++ util/clone.py
@@ -1,45 +1,37 @@
 import json, sys
-print(sys.argv[1])
+print(sys.argv[1], sys.argv[2])
 
-repository_id = sys.argv[1]
 
-with open('clone.json', 'r') as fh:
-    now = json.load(fh)
+repository_id = sys.argv[2]
+
+# with open('clone.json', 'r') as fh:
+#     now = json.load(fh)
 with open('clone_before.json', 'r') as fh:
     before = json.load(fh)
 with open('updated_clone.json', 'r') as fh:
     updated = json.load(fh)
 
+if repository_id not in before.keys():
+    before[repository_id] = {}
 
-latest = dict(before)
-if 'count_past_to_last_2_weeks_ago' not in latest.keys():
-    latest['count_past_to_last_2_weeks_ago'] = 0
-if 'uniques_past_to_last_2_weeks_ago' not in latest.keys():
-    latest['uniques_past_to_last_2_weeks_ago'] = 0
-if 'count_total' not in latest.keys():
-    latest['count_total'] = 0
-if 'uniques_total' not in latest.keys():
-    latest['uniques_total'] = 0
+if 'clones' not in before[repository_id].keys():
+    before[repository_id]['clones'] = []
 
-timestamps = {latest['clones'][i]['timestamp']: i for i in range(len(latest['clones']))}
-for i in range(len(now['clones'])):
-    timestamp = now['clones'][i]['timestamp']
-    if timestamp in timestamps:
-        latest['clones'][timestamps[timestamp]] = now['clones'][i]
+date_list = [t['timestamp'] for t in before[repository_id]['clones']]
+for i in updated['clones']:
+    if i['timestamp'] not in date_list:
+        before[repository_id]['clones'].append(i)
     else:
-        latest['clones'].append(now['clones'][i])
+        for b_clones in before[repository_id]['clones']:
+            # 以最後的為主
+            if b_clones['timestamp'] == i['timestamp']:
+                b_clones['count'] = i['count']
+                b_clones['uniques'] = i['uniques']
 
-if len(latest["clones"]) > 14:
-    temp_list = latest["clones"][:-14]
-    latest["clones"] = latest["clones"][-14:]
-    for i in temp_list:
-        latest['count_past_to_last_2_weeks_ago'] += i['count']
-        latest['uniques_past_to_last_2_weeks_ago'] += i['uniques']
-
-latest['count'] = sum(map(lambda x: int(x['count']), latest['clones']))
-latest['uniques'] = sum(map(lambda x: int(x['uniques']), latest['clones']))
-latest['count_total'] = latest['count_past_to_last_2_weeks_ago'] + latest['count']
-latest['uniques_total'] = latest['uniques_past_to_last_2_weeks_ago'] + latest['uniques']
+before[repository_id]['count'] = sum([x['count'] for x in before[repository_id]['clones']])
+before[repository_id]['uniques'] = sum([x['uniques'] for x in before[repository_id]['clones']])
 
 with open('clone.json', 'w', encoding='utf-8') as fh:
-    json.dump(latest, fh, ensure_ascii=False, indent=4)
+    json.dump(before, fh, ensure_ascii=False, indent=4)
+with open('clone_before.json', 'w', encoding='utf-8') as fh:
+    json.dump(before, fh, ensure_ascii=False, indent=4)
